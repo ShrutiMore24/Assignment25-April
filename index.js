5c9da8223b8881998322e705014034dca7d092e1
ShrutiMore24 <97089262+ShrutiMore24@users.noreply.github.com>
2022-05-01 15:40:58 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,50 @@
+const homeBtn = document.querySelector("#home")
+const booksBtn = document.querySelector("#books")
+const kindleBtn = document.querySelector("#kindle")
+
+const homePara = document.querySelector("#home-p")
+const booksPara = document.querySelector("#books-p")
+const kindlePara = document.querySelector("#kindle-p")
+
+homeBtn.addEventListener('click', displayHome)
+
+function displayHome() {
+    homeBtn.classList.add('btn-active')
+        // homePara.classList.remove("inactive")
+        // homePara.classList.add('active')
+    homePara.classList.toggle('inactive')
+}
+
+booksBtn.addEventListener('click', displayBooks)
+
+function displayBooks() {
+    booksBtn.classList.add('btn-active')
+    booksPara.classList.toggle('inactive')
+
+    homePara.classList.add('inactive')
+    homeBtn.classList.remove('btn-active')
+
+    kindlePara.classList.add('inactive')
+    kindleBtn.classList.remove('btn-active')
+}
+
+
+
+
+// ----------------------------------------------------------------------------------------------------
+// Assignmen Answer
+
+kindleBtn.addEventListener('click', displaykindle)
+
+function displaykindle() {
+    kindleBtn.classList.add('btn-active')
+    kindlePara.classList.toggle('inactive')
+
+    homePara.classList.add('inactive')
+    homeBtn.classList.remove('btn-active')
+
+    booksPara.classList.add('inactive')
+    booksBtn.classList.add('inactive')
+}
+
+// -------------------------------------------------------------------------------------------------------
